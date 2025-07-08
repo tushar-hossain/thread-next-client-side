6b012208804c9129eb0ef8308fbaae52711aa483
Tushar-Hossain <tusharhossain455@gmail.com>
2025-07-09 01:05:27 +0600

    firebase api key store in dotenv file and create authContext and auth provider done

---
+++ src/AuthContext/AuthProvider.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../firebase/firebase.init";
+
+const provider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // login user
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // update user
+  const updateUser = (userObj) => {
+    return updateProfile(auth.currentUser, userObj);
+  };
+
+  // login with google
+  const loginWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+
+  // logout user
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  const userInfo = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    loginUser,
+    updateUser,
+    loginWithGoogle,
+    logOut,
+  };
+
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
