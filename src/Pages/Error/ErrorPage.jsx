25ec676fcda88a1583e67c14d877afabc744f657
tushar-hossain <tusharhossain455@gmail.com>
2025-08-17 22:03:03 +0600

    error page home button added done

--- src/Pages/Error/ErrorPage.jsx
+++ src/Pages/Error/ErrorPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRouteError } from "react-router";
+import { Link, useRouteError } from "react-router";
 import errorAnimation from "../../assets/lottieFile/error.json";
 import Lottie from "lottie-react";
 import { Helmet } from "react-helmet-async";
@@ -16,6 +16,12 @@ const ErrorPage = () => {
         style={{ width: 400 }}
         animationData={errorAnimation}
       />
+
+      <Link className="flex items-center justify-center" to={"/"}>
+        <button className="bg-sky-500 hover:bg-sky-600 text-white px-6 py-2 rounded-lg font-medium transition-all duration-200 flex items-center gap-2 w-fit">
+          Go to Home
+        </button>
+      </Link>
 
       <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold text-center">
         {error.data}
